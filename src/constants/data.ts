dc09d0548fe385b55daff1f8e152694e20dead07
Rimao416 <kayumbaomari27@gmail.com>
2024-10-23 14:03:31 +0200

    MyPush

--- src/constants/data.ts
+++ src/constants/data.ts
@@ -1,3 +1,6 @@
+import Livraison from "../assets/livraison.png"
+import Fast from "../assets/fast-food.png"
+import Chef from "../assets/chef.png"
 // Définition d'un type pour un plat unique
 interface Dish {
   name: string; // Nom du plat
@@ -9,7 +12,7 @@ interface Dish {
 }
 
 // Définition d'un type pour une catégorie de plats (Ex: Poulet, Bœuf)
-interface MenuCategory {
+export interface MenuCategory {
   categoryName: string; // Nom de la catégorie (ex: "Plats de Poulet")
   dishes: Dish[]; // Liste de plats dans la catégorie
 }
@@ -552,3 +555,29 @@ export const myMenu: Menu = {
     // Ajoute d'autres catégories ici...
   ],
 };
+
+// WHY CHOOSE US
+interface WhyChooseUs {
+  title: string;
+  description: string;
+  image: string;
+}
+
+export const WHY_CHOOSE_US: WhyChooseUs[] = [
+  {
+    title: "Livraison gratuite 24/7",
+    description: "Livraison offerte pour toute commande supérieure à 100€.",
+    image: Livraison,
+  },
+  {
+    title: "Chef de renommée",
+    description: "Des plats préparés par un chef primé.",
+    image: Chef,
+  },
+  {
+    title: "Des aliments frais et sains",
+    description: "Des produits frais et équilibrés, à votre table.",
+    image: Fast,
+  },
+
+];
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,7 +5,17 @@ import Logo from "../assets/logo_small.png";
 import Matiti from "../assets/pic4.png";
 import Brochette from "../assets/pic1.png";
 import Button from "../components/Button";
+import { MenuCategory, myMenu, WHY_CHOOSE_US } from "../constants/data";
+import { FaPlus } from "react-icons/fa6";
+import { IoArrowBackSharp } from "react-icons/io5";
+import { IoArrowForwardOutline } from "react-icons/io5";
 
+import { Swiper, SwiperSlide } from "swiper/react";
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import { Autoplay } from "swiper/modules";
+import { useState } from "react";
 
 const menuItems = [
   { label: "Accueil", link: "#" },
@@ -13,9 +23,25 @@ const menuItems = [
   { label: "Avantages", link: "#" },
   { label: "Temoignages", link: "#" },
 ];
-
+const truncateTitle = (title: string | undefined, length: number = 20) => {
+  if (title) {
+    if (title.length > length) {
+      return title.substring(0, length) + "...";
+    } else {
+      return title;
+    }
+  }
+};
 function Home() {
-  // const imagesHeader = [Header_1, Header_2, Header_3];
+  // const imagesHeader = [Header_1, Header_2,
+  // const random_menu=myMenu.categories[Math.floor(Math.random() * myMenu.categories.length)];
+  // console.log(random_menu)
+
+  const random_menu = useState<MenuCategory>(
+    myMenu.categories[Math.floor(Math.random() * myMenu.categories.length)]
+  );
+  console.log(random_menu);
+
   return (
     <div className="app">
       <section className="navigation">
@@ -47,17 +73,77 @@ function Home() {
       </section>
       <section className="header">
         <div className="header__bigline"></div>
-        <img src={Matiti} alt={Matiti} className="header__matiti"/>
+        <img src={Matiti} alt={Matiti} className="header__matiti" />
         <div className="header__wrapper">
-        <p className="header__thumbnail">Explorer le monde des délices</p>
-        <h1 className="header__title">Des aliments qui vous rendent heureux et en bonne santé</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum quia illum architecto id numquam culpa eligendi eos recusandae</p>
-        <div className="header__button">
-          <Button text="Reserver" type="outline" onClick={() => {}} />
-          <Button text="En savoir plus" type="inline" onClick={() => {}} />
+          <p className="header__thumbnail">Explorer le monde des délices</p>
+          <h1 className="header__title">
+            Des saveurs uniques à partager autour de moments précieux.
+          </h1>
+          <p>
+            Une expérience culinaire où chaque plat raconte une histoire,
+            alliant saveurs uniques et moments de partage inoubliables.
+          </p>
+          <div className="header__button">
+            <Button text="Reserver" type="outline" onClick={() => {}} />
+            <Button text="En savoir plus" type="inline" onClick={() => {}} />
+          </div>
         </div>
+        <img src={Brochette} alt={Brochette} className="header__brochette" />
+      </section>
+      <section className="why">
+        {WHY_CHOOSE_US.map((item) => (
+          <div className="why__item" key={item.title}>
+            <img src={item.image} alt={item.title} />
+            <div className="why__text">
+              <h3>{item.title}</h3>
+              <p>{item.description}</p>
+            </div>
+          </div>
+        ))}
+      </section>
+      <section className="daily">
+        <div className="daily__header">
+          <h1>Menu Du Jour</h1>
+          <div className="daily__line">
+            <span className="daily__icon">
+              <IoArrowBackSharp />
+            </span>
+            <span className="daily__icon">
+              <IoArrowForwardOutline />
+            </span>
+          </div>
         </div>
-        <img src={Brochette} alt={Brochette} className="header__brochette"/>
+
+        <div className="daily__wrapper">
+          <Swiper
+            modules={[Autoplay]}
+            loop
+            slidesPerView={4}
+            spaceBetween={40}
+            speed={2000}
+            autoplay={{
+              delay: 2000,
+              disableOnInteraction: false,
+            }}
+          >
+            {random_menu[0].dishes.map((item, index) => (
+              <SwiperSlide key={index}>
+                <div className="daily__item">
+                  <div className="daily__item--wrapper">
+                    <h4 className="daily__item--title">
+                      {truncateTitle(item.name)}
+                    </h4>
+                    <p className="daily__item--description">
+                      {truncateTitle(item.description)}
+                    </p>
+                  </div>
+                  <p className="daily__item--price">{item.prices[0].price} PNL</p>
+                </div>
+                <span className="daily__item--plus">  <FaPlus /></span>
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </div>
       </section>
     </div>
   );
